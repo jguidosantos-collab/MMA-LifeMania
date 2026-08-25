f530823c36d6d065654a8a43ada04be38298f5ed
jguidosantos-collab <jguidosantos@gmail.com>
2026-08-25 11:50:10 -0300

    Delete js directory

--- js/data/teams.js
+++
@@ -1,7 +0,0 @@
-export const TEAMS = [
-{ id: "regional", name: "Academia Regional", quality: 45 },
-{ id: "medium", name: "Equipe Mediana", quality: 60 },
-{ id: "national", name: "Equipe Nacional", quality: 72 },
-{ id: "international", name: "Equipe Internacional", quality: 84 },
-{ id: "elite", name: "Equipe Elite", quality: 95 }
-];
