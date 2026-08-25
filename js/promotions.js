4459f40340b07db321fbedcc007394a5f705ae0d
jguidosantos-collab <jguidosantos@gmail.com>
2026-08-25 16:23:18 -0300

    Create promotions.js with promotion data
    Added a list of promotions with details such as name, country, level, prestige, and base purse.

---
+++ js/promotions.js
@@ -0,0 +1,154 @@
+const promotions = [
+
+    {
+        id: 1,
+        name: "Circuito Regional de Combate",
+        country: "Brasil",
+        level: 1,
+        prestige: 10,
+        international: false,
+        basePurse: 500
+    },
+
+    {
+        id: 2,
+        name: "Brazil Combat League",
+        country: "Brasil",
+        level: 2,
+        prestige: 20,
+        international: false,
+        basePurse: 1200
+    },
+
+    {
+        id: 3,
+        name: "Arena Nacional MMA",
+        country: "Brasil",
+        level: 3,
+        prestige: 30,
+        international: false,
+        basePurse: 2500
+    },
+
+    {
+        id: 4,
+        name: "Warrior Challenge",
+        country: "Estados Unidos",
+        level: 3,
+        prestige: 35,
+        international: true,
+        basePurse: 3000
+    },
+
+    {
+        id: 5,
+        name: "Titan Fight Championship",
+        country: "México",
+        level: 4,
+        prestige: 40,
+        international: true,
+        basePurse: 4500
+    },
+
+    {
+        id: 6,
+        name: "Prime Combat League",
+        country: "Canadá",
+        level: 4,
+        prestige: 45,
+        international: true,
+        basePurse: 6000
+    },
+
+    {
+        id: 7,
+        name: "Continental Warriors",
+        country: "Europa",
+        level: 5,
+        prestige: 50,
+        international: true,
+        basePurse: 7500
+    },
+
+    {
+        id: 8,
+        name: "Global Fight Series",
+        country: "Internacional",
+        level: 5,
+        prestige: 55,
+        international: true,
+        basePurse: 9000
+    },
+
+    {
+        id: 9,
+        name: "World Combat League",
+        country: "Internacional",
+        level: 6,
+        prestige: 60,
+        international: true,
+        basePurse: 12000
+    },
+
+    {
+        id: 10,
+        name: "Elite Combat Championship",
+        country: "Estados Unidos",
+        level: 6,
+        prestige: 65,
+        international: true,
+        basePurse: 15000
+    },
+
+    {
+        id: 11,
+        name: "International Fight League",
+        country: "Internacional",
+        level: 7,
+        prestige: 70,
+        international: true,
+        basePurse: 20000
+    },
+
+    {
+        id: 12,
+        name: "World Warriors Championship",
+        country: "Internacional",
+        level: 7,
+        prestige: 75,
+        international: true,
+        basePurse: 25000
+    },
+
+    {
+        id: 13,
+        name: "Global Elite Fighting",
+        country: "Internacional",
+        level: 8,
+        prestige: 82,
+        international: true,
+        basePurse: 35000
+    },
+
+    {
+        id: 14,
+        name: "World MMA Championship",
+        country: "Internacional",
+        level: 9,
+        prestige: 90,
+        international: true,
+        basePurse: 50000
+    },
+
+    {
+        id: 15,
+        name: "Sovereign Fighting Championship",
+        country: "Internacional",
+        level: 10,
+        prestige: 100,
+        international: true,
+        basePurse: 100000,
+        elite: true
+    }
+
+];
